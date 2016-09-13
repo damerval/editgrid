8fa3e99a9ad53dbd6d19d88d830b9b75abf9231e
damerval <damerval@gmail.com>
2016-09-13 15:24:06 -0800

    Ajax stuff

--- savePhrase.php
+++ savePhrase.php
@@ -10,15 +10,19 @@ require_once("dataFunctions.php");
 if (isset($_GET['phrase_text']) && isset($_GET['operation'])) {
   $phraseText = $_GET['phrase_text'];
   $dateField = isset($_GET['date_field']) ? $_GET['date_field'] : null;
+  $numberField = isset($_GET['numberField']) ? $_GET['numberField'] : null;
   $operation = $_GET['operation'];
   if ($operation == "insert") {
-    echo get_result_cud("INSERT INTO phrases (phrase, datefield) VALUES ('" . addslashes(urldecode($phraseText)) . "', CURDATE())", $operation);
+    echo get_result_cud("INSERT INTO phrases (phrase, datefield, numberField) VALUES ('"
+        . addslashes(urldecode($phraseText))
+        . "', CURDATE(), 0)", $operation);
   } elseif (isset($_GET['row_id'])) {
     $rowId = $_GET['row_id'];
     if ($operation == "update") {
       echo get_result_cud("UPDATE phrases SET phrase = '"
           . addslashes(urldecode($phraseText)) . "', datefield = '"
-          . $dateField . "' WHERE phrase_id = " . $rowId, $operation);
+          . $dateField . "', numberField = "
+          . $numberField.  " WHERE phrase_id = " . $rowId, $operation);
     } elseif ($operation == "delete") {
       echo get_result_cud("DELETE FROM phrases WHERE phrase_id = " . $rowId, $operation);
     } else {
