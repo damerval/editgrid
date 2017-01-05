8f6254df71309491c504a50af63864d15c73c988
damerval <damerval@gmail.com>
2017-01-04 16:25:09 -0900

    Add events for drop down selection and make window close / button enable or disable more readable

--- tests/stage_events.js
+++ tests/stage_events.js
@@ -2,14 +2,41 @@
  * Created by pdamerval on 12/30/2016.
  */
 
+function checkColorAssignmentsDialog() {
+  return cawOffenderSelect.jqxDropDownList('selectedIndex') !== -1
+      && cawColorSelect.jqxDropDownList('selectedIndex') !== -1
+      && cawStartDate.val() !== "" ;
+}
+
+function enableButton(buttonObj) {
+  buttonObj.jqxButton({ disabled: false });
+}
+
+function disableButton(buttonObj) {
+  buttonObj.jqxButton({ disabled: true });
+}
+
+function openWindow(windowObj) {
+  windowObj.jqxWindow('open');
+}
+
 $(document).ready(function() {
 
   colorAssignmentAddButton.on('click', function() {
-    colorAssignmentWindow.jqxWindow('open');
+    openWindow(colorAssignmentWindow);
+    disableButton(cawSaveButton);
   });
 
-  cawSaveButton.on('click', function() {
-    alert(cawOffenderSelect.val());
+  cawOffenderSelect.on('select', function () {
+    if (checkColorAssignmentsDialog()) enableButton(cawSaveButton);
+  });
+
+  cawColorSelect.on('select', function () {
+    if (checkColorAssignmentsDialog()) enableButton(cawSaveButton);
+  });
+
+  cawSaveButton.on('click', function () {
+    alert(cawOffenderSelect.val() + " | " + cawColorSelect.val())
   });
 
 });
